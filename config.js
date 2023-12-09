9aded9e7367f8047e52ff11b46e76e1c8559daf3
mimi553 <127617461+mimi553@users.noreply.github.com>
2023-12-09 09:35:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
+global.owner = [mimi
+  ['917605902011', ', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of maniac
